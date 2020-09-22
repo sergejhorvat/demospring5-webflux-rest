58370db20153123af0f266c9555384bb18261a29
Sergej Horvat <sergejhorvat@users.noreply.github.com>
2020-09-22 16:13:34 +0200

    Initial commit. Project setup.

---
+++ src/main/java/inc/inhuman/demospring5webfluxrest/Demospring5WebfluxRestApplication.java
@@ -0,0 +1,13 @@
+package inc.inhuman.demospring5webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospring5WebfluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demospring5WebfluxRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inc/inhuman/demospring5webfluxrest/Demospring5WebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.inhuman.demospring5webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demospring5WebfluxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
